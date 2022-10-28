0ec1d3222bbfda068ebf5f22350afb2f30c81f74
SanjotD <112981867+SanjotD@users.noreply.github.com>
2022-10-28 14:47:27 -0600

    questions

--- main.js
+++ main.js
@@ -1,79 +1,106 @@
-// // JavaScript Code
+// JavaScript Code
 
 
-// // Input
+ // Input
 
 
-// // Variables
+ // Variables
 
-// let outputMessageEl = document.getElementById("output");
+let outputMessageEl = document.getElementById("output");
 
-// document.getElementById('btn').addEventListener('click', calculateScore);
+document.getElementById('btn').addEventListener('click', calculateScore);
 
-
-
-
+let score = 0;
 
 
-// function calculateScore() {
+
+function calculateScore() {
 // // Process
-//         let firstQuestEl = document.getElementById("peaceQuest").value.toLowerCase();
-//         let secondQuestEl = document.getElementById("aloeQuest").value.toLowerCase();
-//         let thirdQuestEl = document.getElementById("lemonQuest").value.toLowerCase();
-//         let fourthQuestEl = document.getElementById("houseQuest").value.toLowerCase();
+        let firstQuestEl = document.getElementById("peaceQuest").value.toLowerCase();
+        let secondQuestEl = +document.getElementById("aloeQuest").value;
+        let thirdQuestEl = document.getElementById("lemonQuest").value.toLowerCase();
+        let fourthQuestEl = document.getElementById("houseQuest").value.toLowerCase();
 
-//  firstQuestEl = 0;
-//  secondQuestEl = 0;
-//  thirdQuestEl = 0;
-//  fourthQuestEl = 0;
+       
 
 
-// if (firstQuestEl === "no" || firstQuestEl === "nope"){ 
-//     firstQuestEl++;
+
+
+if (firstQuestEl === "no" || firstQuestEl === "nope"|| firstQuestEl === "n"){ 
+    score++;
+    console.log(score);
+    document.getElementById("box1").src = "img/checkedbox.jpg";
+}else {
+    document.getElementById("box1").src = "img/xmarkcheckedbox.jpg"
+}
+
+if (secondQuestEl == "99%" || secondQuestEl == "99"|| secondQuestEl == " 99"|| secondQuestEl == "99 "){
+
+    score++;
+    console.log(score);
+    document.getElementById("box2").src = "img/checkedbox.jpg";
+
+
+
+}
+if (thirdQuestEl === "no" || secondQuestEl === "nope"|| secondQuestEl === "n"){
+    score++;
+    console.log(score);
     
-// } else if (secondQuestEl === "99%" || secondQuestEl === "99"|| secondQuestEl === " 99"|| secondQuestEl === "9 9"){
-//     secondQuestEl++;
+
+}
+ if (fourthQuestEl === "66%" || secondQuestEl === "66"|| secondQuestEl === " 66" || secondQuestEl === "66"){
+    score++;
+    console.log(score);
+
+}
+
+else {
     
+    document.getElementById("box2").src = "img/xmarkcheckedbox.jpg"
+    document.getElementById("box3").src = "img/xmarkcheckedbox.jpg"
+    document.getElementById("box4").src = "img/xmarkcheckedbox.jpg"
+}
+// else {
+//     firstQuestEl = 0;
+//     secondQuestEl = 0;
+//     thirdQuestEl = 0;
+//     fourthQuestEl = 0;
 // }
-// else if (thirdQuestEl === "no" || secondQuestEl === "nope"){
-//     thirdQuestEl++;
-    
-// }
-// else if (fourthQuestEl === "66%" || secondQuestEl === "66"|| secondQuestEl === " 66" || secondQuestEl === "6 6"){
-//     fourthQuestEl++;
-    
-// }
 
 
-// let outOfScoreEl =
-//  (firstQuestEl + secondQuestEl + thirdQuestEl + fourthQuestEl);
+let outOfScoreEl =
+ (score <=4);
  
 
-// let percentScoreEl =
-//  (firstQuestEl + secondQuestEl + thirdQuestEl + fourthQuestEl) / 4;
+let percentScoreEl =
+ ((score) / 4) * 100;
 
 
-// if (outOfScoreEl <= 2) {
-//     outputMessageEl.innerHTML = ("Not bad!");
-// }
+if (outOfScoreEl = 2) {
+    outputMessageEl.innerHTML = ("Not bad!");
+}
 
-//  else if (outOfScoreEl <= 3) {
-//     outputMessageEl.innerHTML = ("So Close!");
-// }
+ else if (outOfScoreEl = 3) {
+    outputMessageEl.innerHTML = ("So Close!");
+}
 
-// else if (outOfScoreEl <= 4) {
-//     outputMessageEl.innerHTML = ("Good Job!");
-// }
+else if (outOfScoreEl = 4) {
+    outputMessageEl.innerHTML = ("Amazing Job!");
+}
+
+else {
+    outputMessageEl.innerHTML = ("Try Again!")
+}
 
 
 
-// //Output
-// document.getElementById("outOfScore").innerHTML = outOfScoreEl;
-// document.getElementById("percentScore").innerHTML = percentScoreEl.toFixed();
+//Output
+document.getElementById("outOfScore").innerHTML = outOfScoreEl;
+document.getElementById("percentScore").innerHTML = percentScoreEl.toFixed();
 
-// console.log(firstQuestEl)
 
-// }
+}
 
 
 // --------------------------------------------------------ATTEMPT 2
@@ -82,33 +109,41 @@
 
 
 
-// Variables
-let outputMessageEl = document.getElementById("output");
-let boxCheck1 = document.getElementById("box1");
-// let boxCheck2 = document.getElementById("box2");
-// let boxCheck3 = document.getElementById("box3");
-// let boxCheck4 = document.getElementById("box4");
+// // Variables
+// let outputMessageEl = document.getElementById("output");
+// let boxCheck1 = document.getElementById("box1");
+// // // let boxCheck2 = document.getElementById("box2");
+// // // let boxCheck3 = document.getElementById("box3");
+// // let boxCheck4 = document.getElementById("box4");
 
-let outOfScoreEl = document.getElementById("outOfScore");
-let percentScoreEl = document.getElementById("percentScore");
+// let outOfScoreEl = document.getElementById("outOfScore");
+// let percentScoreEl = document.getElementById("percentScore");
 
-let question1El = document.getElementById("peaceQuest").value.toLowerCase();
-let question2El = document.getElementById("aloeQuest").value.toLowerCase();
-let question3El = document.getElementById("lemonQuest").value.toLowerCase();
-let question4El = document.getElementById("houseQuest").value.toLowerCase();
+// let question1El = document.getElementById("peaceQuest").value.toLowerCase();
+// let question2El = document.getElementById("aloeQuest").value.toLowerCase();
+// let question3El = document.getElementById("lemonQuest").value.toLowerCase();
+// let question4El = document.getElementById("houseQuest").value.toLowerCase();
 
-document.getElementById("btn").addEventListener("click", testScore)
+// document.getElementById("btn").addEventListener("click", testScore)
 
-function testScore() {
-// Process
+// function testScore() {
+// // // Process
 
-if (question1El === "no" || question1El === "nope" || question1El === "n") {
-boxCheck1.src = 'img/checkedbox.jpg'
+// question1El = 1;
+// question2El = 1;
+// question3El = 1;
+// question4El= 1;
 
-}
+
+// if (question1El === "no" || question1El === "nope" || question1El === "n" || question2El === "99%" || question2El === "99" || question3El === "no" || question3El === "n" || question4El === "66%" || question4El === "66") {
+
+// outputMessageEl.innerHTML = ('Amazing!');
+// boxCheck1.src = 'img/checkedbox.jpg';
+
+// }
 
 
-// Output
-console.log(question1El)
+// // // Output
+// console.log(testScore)
 
-}
+// }
