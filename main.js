c45691bc516e03aaace94c066aa4b7c344f0a03c
SanjotD <112981867+SanjotD@users.noreply.github.com>
2022-11-03 14:20:06 -0600

    fix refresh--almost fin

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function calculateScore() {
         let secondQuestEl = +document.getElementById("aloeQuest").value;
         let thirdQuestEl = document.getElementById("lemonQuest").value.toLowerCase();
         let fourthQuestEl = +document.getElementById("houseQuest").value;
-
+    
        
 
 
@@ -28,7 +28,6 @@ function calculateScore() {
 
 if (firstQuestEl === "no" || firstQuestEl === "nope"|| firstQuestEl === "n"){ 
     score++;
-    console.log(score);
     document.getElementById("box1").src = "img/checkedbox.jpg";
 } else {
     document.getElementById("box1").src = "img/xmarkcheckedbox.jpg"
@@ -37,7 +36,6 @@ if (firstQuestEl === "no" || firstQuestEl === "nope"|| firstQuestEl === "n"){
 
 if (secondQuestEl == "99%" || secondQuestEl == "99"|| secondQuestEl == " 99"|| secondQuestEl == "99 "){
     score++;
-    console.log(score);
     document.getElementById("box2").src = "img/checkedbox.jpg";
 } else {
     document.getElementById("box2").src = "img/xmarkcheckedbox.jpg"
@@ -47,7 +45,6 @@ if (secondQuestEl == "99%" || secondQuestEl == "99"|| secondQuestEl == " 99"|| s
 if (thirdQuestEl === "no" || thirdQuestEl === "nope"|| thirdQuestEl === "n"){
     score++;
     document.getElementById("box3").src = "img/checkedbox.jpg";
-    console.log(score);
 } else {
     document.getElementById("box3").src = "img/xmarkcheckedbox.jpg"
 }
@@ -56,12 +53,41 @@ if (thirdQuestEl === "no" || thirdQuestEl === "nope"|| thirdQuestEl === "n"){
 if (fourthQuestEl == "66%" || fourthQuestEl == "66"|| fourthQuestEl == " 66" || fourthQuestEl == "66"){
     score++;
     document.getElementById("box4").src = "img/checkedbox.jpg";
-    console.log(score);
 }else {  
     document.getElementById("box4").src = "img/xmarkcheckedbox.jpg";
 }
 
+console.log(score);
+
+let percentScoreEl =
+ ((score) / 4) * 100;
+
+let outOfScoreEl = (score)
+ console.log(outOfScoreEl)
+
+
+if (outOfScoreEl <= 1) {
+    outputMessageEl.innerHTML = ("Try Again.");
 }
+
+else if (outOfScoreEl <= 2){
+    outputMessageEl.innerHTML = ("Not bad!");
+}
+
+ else if (outOfScoreEl <= 3) {
+    outputMessageEl.innerHTML = ("So Close!");
+}
+
+else if (outOfScoreEl <= 4) {
+    outputMessageEl.innerHTML = ("Amazing Job!");
+}
+document.getElementById("outOfScore").innerHTML = outOfScoreEl;
+document.getElementById("percentScore").innerHTML = percentScoreEl.toFixed();
+
+}
+
+
+
 
 
 
@@ -79,34 +105,13 @@ if (fourthQuestEl == "66%" || fourthQuestEl == "66"|| fourthQuestEl == " 66" || 
 
  
 
-let percentScoreEl =
- ((score) / 4) * 100;
 
- console.log(outOfScoreEl)
-
-
-if (outOfScoreEl <= 2) {
-    outputMessageEl.innerHTML = ("Not bad!");
-}
-
- else if (outOfScoreEl = 3) {
-    outputMessageEl.innerHTML = ("So Close!");
-}
-
-else if (outOfScoreEl = 4) {
-    outputMessageEl.innerHTML = ("Amazing Job!");
-}
-
-else {
-    outputMessageEl.innerHTML = ("Try Again!")
-}
 
 
 
 //Output
 
-document.getElementById("outOfScore").innerHTML = outOfScoreEl;
-document.getElementById("percentScore").innerHTML = percentScoreEl.toFixed();
+
 
 
 
